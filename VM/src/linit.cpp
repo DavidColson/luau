b11cff1781dc69f3edcb6e30e5289da23f1e0205
David Colson <davidcncolson@gmail.com>
2024-08-03 18:38:39 +0100

    Removed coroutine library, not appropriate for polybox

--- VM/src/linit.cpp
+++ VM/src/linit.cpp
@@ -6,7 +6,6 @@
 
 static const luaL_Reg lualibs[] = {
     {"", luaopen_base},
-    {LUA_COLIBNAME, luaopen_coroutine},
     {LUA_TABLIBNAME, luaopen_table},
     {LUA_OSLIBNAME, luaopen_os},
     {LUA_STRLIBNAME, luaopen_string},
